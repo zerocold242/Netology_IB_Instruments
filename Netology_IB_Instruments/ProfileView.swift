b7dcabb21181f8b96a977f993217fb438f44a1b3
zerocold242 <alexrylexx@gmail.com>
2022-03-13 16:55:40 +0300

    outlets

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Aleksey Lexx on 13.03.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    @IBOutlet weak var userName: UILabel!
+    
+    @IBOutlet weak var birthDate: UILabel!
+    
+    @IBOutlet weak var city: UILabel!
+    
+    @IBOutlet weak var destination: UITextView!
+}
